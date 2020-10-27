b36d0367a360f8dc6146bd0ff8c40d3d957c4456
kotborealis <kotborealis@gmail.com>
2020-10-27 21:45:15 +0300

    --superres, --gui and samples

--- wbd.py
+++ wbd.py
@@ -3,12 +3,16 @@ from wbd import board_calibration, board_transform
 import cv2 as cv
 import numpy as np
 import argparse
+from ISR.models import RDN
+from PIL import Image
 
 ap = argparse.ArgumentParser()
 ap.add_argument('--image', help='Path to image file')
 ap.add_argument('--calibrate-board', help='Calibrate board points and save to specified file', default=False)
 ap.add_argument('--transform-board', help='Transform board using points from specified file(s)', nargs='*')
 ap.add_argument('--save-as', help='Save transformed boards to specified file(s)', nargs='*')
+ap.add_argument('--superres', help='Apply super-resolution (slow!)', default=False, action='store_true')
+ap.add_argument('--gui', help='Enable gui', default=False, action='store_true')
 args = vars(ap.parse_args())
 
 if args["calibrate_board"]:
@@ -20,17 +24,29 @@ if args["calibrate_board"]:
     else:
         print("Expected 4 points, got " + str(len(points)))
 elif args["transform_board"]:
+    if args["superres"]:
+        rdn = RDN(weights='psnr-small')
+
     original = cv.imread(args["image"])
-    cv.imshow(args["image"], original)
+    if args["gui"]:
+        cv.imshow(args["image"], original)
 
     idx = 0
     for transform in args["transform_board"]:
         with open(transform) as f:
             points = np.array(json.load(f), dtype="float32")
-            result = board_transform.four_point_transform(original, points)
+            crop = board_transform.four_point_transform(original, points)
+
+            if args["superres"]:
+                result = rdn.predict(crop)
+            else:
+                result = crop
+
+            if args["gui"]:
+                cv.imshow(transform, result)
+
             if args["save_as"] and len(args["save_as"]) > idx:
                 cv.imwrite(args["save_as"][idx], result)
 
-            cv.imshow(transform, result)
         idx = idx + 1
     cv.waitKey(0)
