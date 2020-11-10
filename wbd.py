0837430120db43a7e77854428c7121018614ab5a
a <a@awooo.ru>
2020-11-10 14:18:55 +0300

    dev

--- wbd.py
+++ wbd.py
@@ -6,18 +6,34 @@ import argparse
 import requests
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--image-path', help='Path to image file')
-ap.add_argument('--image-url', help='Image URL')
+ap.add_argument('--image-path', help='Grab image from specified file')
+ap.add_argument('--image-url', help='Grab image from specified URL')
+ap.add_argument('--image-rtsp', help='Grab image from specified RTSP stream')
 ap.add_argument('--calibrate', help='Calibrate board points using GUI and save to specified file', default=False)
 ap.add_argument('--transform', help='Transform board using points from specified file(s)', nargs='*')
-ap.add_argument('--superres', help='Apply super-resolution (slow!)', default=False, action='store_true')
-ap.add_argument('--gui', help='Enable gui', default=False, action='store_true')
 ap.add_argument('--output', help='Save transformed boards to specified file(s)', nargs='*')
 ap.add_argument('--output-original', help='Save original to specified file(s)')
 args = vars(ap.parse_args())
 
+original = []
+
+if args["image_path"]:
+    original = cv.imread(args["image_path"])
+elif args["image_url"]:
+    res = requests.get(args["image_url"], stream=True).content
+    original = np.asarray(bytearray(res), dtype="uint8")
+    original = cv.imdecode(original, cv.IMREAD_COLOR)
+elif args["image_rtsp"]:
+    res, frame = cv.VideoCapture(args["image_rtsp"]).read()
+    if not res:
+        print("Failed to grab frame from specified rtsp stream")
+        exit(1)
+else:
+    print("Neither `--image-path`, `--image-url` or `--image-rtsp` was specified, use `--help` to print usage")
+    exit(1)
+
 if args["calibrate"]:
-    points = board_calibration.board_calibration(cv.imread(args["image_path"]))
+    points = board_calibration.board_calibration(original)
 
     if len(points) == 4:
         with open(args["calibrate"], 'w') as f:
@@ -25,47 +41,18 @@ if args["calibrate"]:
     else:
         print("Expected 4 points, got " + str(len(points)))
 elif args["transform"]:
-    rdn = []
-    if args["superres"]:
-        from ISR.models import RDN
-        rdn = RDN(weights='psnr-small')
-
-    original = []
-
-    if args["image_path"]:
-        original = cv.imread(args["image_path"])
-    elif args["image_url"]:
-        res = requests.get(args["image_url"], stream=True).content
-        original = np.asarray(bytearray(res), dtype="uint8")
-        original = cv.imdecode(original, cv.IMREAD_COLOR)
-    else:
-        print("Neither `--image-path` nor `--image-url` a specified, use `--help` to print usage")
-        exit(1)
-
-    if args["output_original"]:
-        cv.imwrite(args["output_original"], original)
-
-    if args["gui"]:
-        cv.imshow("source", original)
-
     idx = 0
     for transform in args["transform"]:
         with open(transform) as f:
             points = np.array(json.load(f), dtype="float32")
-            crop = board_transform.four_point_transform(original, points)
-
-            if args["superres"]:
-                result = rdn.predict(crop)
-            else:
-                result = crop
-
-            if args["gui"]:
-                cv.imshow(transform, result)
+            result = board_transform.four_point_transform(original, points)
 
             if args["output"] and len(args["output"]) > 0:
                 cv.imwrite(args["output"].pop(0), result)
 
         idx = idx + 1
-    cv.waitKey(0)
-else:
-    print("Use `--help` to print usage")
+
+if args["output_original"]:
+    if args["image_rtsp"]:
+        res, frame = cv.VideoCapture(args["image_rtsp"]).read()
+        cv.imwrite(args["output_original"], frame)
